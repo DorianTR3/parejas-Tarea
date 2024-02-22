44e9f7e51d092b06dc6ade0f7b74d4e718fd5dd8
Antonio Lua <antonio28luah@gmail.com>
2024-02-22 12:06:14 -0600

    Actualizacion 2.0

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
      Scanner leer =new Scanner(System.in);
      String palabra;
+        System.out.println("Agregue una palabra: ");
      palabra=leer.nextLine();
         System.out.println("letras que tiene la palabra: ");
     }
